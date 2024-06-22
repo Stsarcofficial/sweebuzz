cbd3ecf119f64a4fe133467399c35c424d910601
Vaibhav Kale <vaiibhavkale@gmail.com>
2024-06-22 12:33:15 +0530

    Enabling multiDex

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,9 @@ import 'package:sweebuzz/LoginScreen/create_acc_acreen.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:sweebuzz/NotificationScreen/Notification_Allpage.dart';
 
-// Future<void> main() async {
-//   runApp(const MyApp());
-//   await FlutterDisplayMode.setHighRefreshRate();
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const HomeScreen(),  // Set the new home screen here
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-
-void main() {
+Future<void> main() async {
   runApp(MyApp());
+  await FlutterDisplayMode.setHighRefreshRate();
 }
 
 class MyApp extends StatelessWidget {
